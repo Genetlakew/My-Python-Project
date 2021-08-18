a7f816e13a93c086ec1ca76cc0a1dcdfbab0de4a
Genet Lakew <genetlakew28@gmail.com>
2021-08-18 15:32:08 -0400

    folder just fixed

---
+++ lesson-1/list.py
@@ -0,0 +1,28 @@
+
+## List 
+
+numbers = [0, 1, 2, 3, 4, 5]  # all are the same data types - a list of numbers
+fruit = ['Banana', 'Orange', 'Mango', 'Avocado'] # all the same data types - a list of strings (fruits)
+countries = ['Ethiopia','India', 'Sweden','Norway'] # all the same data types - a list of strings (countries)
+mix = ['Banana', 20, False, 9.81] # different data types in the list - string, integer, boolean and float
+print('collection of data')
+
+# Tuple can not be modified
+tpl = ('Genet', 'Selam', 'Smith', 'Abraham', 'Lili') # Names
+print('immutable')
+ # Arithmetic operations in python 
+# Arithmetic Operations in Python
+# Integers
+
+print('Addition: ', 1 + 2)        # 3
+print('Subtraction: ', 2 - 1)     # 1
+print('Multiplication: ', 2 * 3)  # 6
+print ('Division: ', 4 / 2)       # 2.0  Division in Python gives floating number
+print('Division: ', 6 / 2)        # 3.0         
+print('Division: ', 7 / 2)        # 3.5
+print('Division without the remainder: ', 7 // 2)   # 3,  gives without the floating number or without the remaining
+print ('Division without the remainder: ',7 // 3)   # 2
+print('Modulus: ', 3 % 2)         # 1, Gives the remainder
+print('Exponentiation: ', 2 ** 3) # 9 it means 2 * 2 * 2
+ 
+ 
