e9bc21d29752f3391404dd29fc24c600c95fc9a3
Genet Lakew <genetlakew28@gmail.com>
2021-08-20 03:32:19 -0400

    checking floating and list

--- lesson-1/list.py
+++ lesson-1/list.py
@@ -14,15 +14,38 @@ print('immutable')
 # Arithmetic Operations in Python
 # Integers
 
-print('Addition: ', 1 + 2)        # 3
-print('Subtraction: ', 2 - 1)     # 1
-print('Multiplication: ', 2 * 3)  # 6
-print ('Division: ', 4 / 2)       # 2.0  Division in Python gives floating number
-print('Division: ', 6 / 2)        # 3.0         
-print('Division: ', 7 / 2)        # 3.5
-print('Division without the remainder: ', 7 // 2)   # 3,  gives without the floating number or without the remaining
-print ('Division without the remainder: ',7 // 3)   # 2
-print('Modulus: ', 3 % 2)         # 1, Gives the remainder
-print('Exponentiation: ', 2 ** 3) # 9 it means 2 * 2 * 2
- 
+#print('Addition: ', 1 + 2)        # 3
+#print('Subtraction: ', 2 - 1)     # 1
+#print('Multiplication: ', 2 * 3)  # 6
+#print ('Division: ', 4 / 2)       # 2.0  Division in Python gives floating number
+#print('Division: ', 6 / 2)        # 3.0         
+#print('Division: ', 7 / 2)        # 3.5
+#print('Division without the remainder: ', 7 // 2)   # 3,  gives without the floating number or without the remaining
+#print ('Division without the remainder: ',7 // 3)   # 2
+#print('Modulus: ', 3 % 2)         # 1, Gives the remainder
+#print('Exponentiation: ', 2 ** 3) # 9 it means 2 * 2 * 2
+
+# Floating numbers 
+#print('floating point number, PI',3.14)
+#print ('floating point mumber, gravity', 9.81)
+#fruits = ['banana', 'orange', 'mango', 'lemon']
+#fruits.insert(2, 'apple') # insert apple between orange and mango
+#print(fruits)           # ['banana', 'orange', 'apple', 'mango', 'lemon']
+#fruits.insert(3, 'lime')   # ['banana', 'orange', 'apple', 'lime', 'mango', 'lemon']
+#print(fruits)
+
+positive_numbers = [1, 2, 3, 4, 5]
+zero = [0]
+negative_numbers = [-5,-4,-3,-2,-1]
+integers = negative_numbers + zero + positive_numbers
+print(integers) # [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+fruits_and_vegetables = fruits + vegetables
+print(fruits_and_vegetables ) # ['banana', 'orange', 'mango', 'lemon', 'Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+
+# Nested Conditions
+
+
+
  
